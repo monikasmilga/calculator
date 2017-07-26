55001bd50b0b2ce0fe8d172c918ece1d95a93e66
monikasmilga <tuuli705@gmail.com>
2017-07-26 14:32:48 +0300

    :white_check_mark: Correct button attributes; returns correct value when clicked

--- scripts.js
+++ scripts.js
@@ -1,105 +1,104 @@
-
 // array of button objects
 var buttonArray = [
     {
-        type: "action",
-        value: "C",
-        buttonClass: "grey"
+        "type": "action",
+        "value": "C",
+        "buttonClass": "grey"
     },
     {
-        type: "action",
-        value: "<<",
-        buttonClass: "grey"
+        "type": "action",
+        "value": "<<",
+        "buttonClass": "grey"
     },
     {
-        type: "action",
-        value: "%",
-        buttonClass: "grey"
+        "type": "action",
+        "value": "%",
+        "buttonClass": "grey"
     },
     {
-        type: "action",
-        value: "/",
-        buttonClass: "grey"
+        "type": "action",
+        "value": "/",
+        "buttonClass": "grey"
     },
     {
-        type: "number",
-        value: 9,
-        buttonClass: "grey"
+        "type": "number",
+        "value": 9,
+        "buttonClass": "grey"
     },
     {
-        type: "number",
-        value: 8,
-        buttonClass: "grey"
+        "type": "number",
+        "value": 8,
+        "buttonClass": "grey"
     },
     {
-        type: "number",
-        value: 7,
-        buttonClass: "grey"
+        "type": "number",
+        "value": 7,
+        "buttonClass": "grey"
     },
     {
-        type: "action",
-        value: "*",
-        buttonClass: "grey"
+        "type": "action",
+        "value": "*",
+        "buttonClass": "grey"
     },
     {
-        type: "number",
-        value: 4,
-        buttonClass: "grey"
+        "type": "number",
+        "value": 4,
+        "buttonClass": "grey"
     },
     {
-        type: "number",
-        value: 5,
-        buttonClass: "grey"
+        "type": "number",
+        "value": 5,
+        "buttonClass": "grey"
     },
     {
-        type: "number",
-        value: 6,
-        buttonClass: "grey"
+        "type": "number",
+        "value": 6,
+        "buttonClass": "grey"
     },
     {
-        type: "action",
-        value: "-",
-        buttonClass: "grey"
+        "type": "action",
+        "value": "-",
+        "buttonClass": "grey"
     },
     {
-        type: "number",
-        value: 1,
-        buttonClass: "grey"
+        "type": "number",
+        "value": 1,
+        "buttonClass": "grey"
     },
     {
-        type: "number",
-        value: 2,
-        buttonClass: "grey"
+        "type": "number",
+        "value": 2,
+        "buttonClass": "grey"
     },
     {
-        type: "number",
-        value: 3,
-        buttonClass: "grey"
+        "type": "number",
+        "value": 3,
+        "buttonClass": "grey"
     },
     {
-        type: "action",
-        value: "+",
-        buttonClass: "grey"
+        "type": "action",
+        "value": "+",
+        "buttonClass": "grey"
     },
     {
-        type: "number",
-        value: "+-",
-        buttonClass: "grey"
+        "type": "number",
+        "value": "+-",
+        "buttonClass": "grey"
     },
     {
-        type: "number",
-        value: 0,
-        buttonClass: "grey"
+        "type": "number",
+        "value": 0,
+        "buttonClass": "grey"
     },
     {
-        type: "number",
-        value: ".",
-        buttonClass: "grey"
+        "type": "number",
+        "value": ".",
+        "buttonClass": "grey"
     },
     {
-        type: "action",
-        value: "=",
-        buttonClass: "grey"
+        "type": "action",
+        "value": "=",
+        "buttonClass": "grey"
     }
 ];
 
@@ -108,14 +107,14 @@ var buttonArray = [
 
 $(document).ready(function () {
     $('body').prepend("<div id='buttonField'></div>");
-    $('#buttonField').append("<input>");
-    $.each(buttonArray, function ( value, symbol) {
-        $("#buttonField").append("<button>" + symbol.value + "</button>")
-            $('button').attr("class", this.buttonClass).attr("type", this.type).attr("value", this.value);
+    $('#buttonField').append("<input>"); // change to read only, give value
+
+    $.each(buttonArray, function (index, buttonArray) {
+        $("#buttonField").append($("<button>" + buttonArray.value + "</button>").attr("class", buttonArray.buttonClass).attr("type", buttonArray.type).attr("value", buttonArray.value));
     });
 });
 
-// onclick
+// when clicked gets button value
 
 $(document).ready(function () {
     $("button").click(function () {
